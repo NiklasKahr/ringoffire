5ec23e794e7b07ba159b23b82176e43f14689f9d
Niklas Kahr <NiklasKahr>
2023-03-29 14:13:17 +0200

    first release

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -2,12 +2,10 @@ import { Component, OnInit, inject } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Game } from 'src/models/game';
 import { DialogAddPlayerComponent } from '../dialog-add-player/dialog-add-player.component';
-import { Firestore, collection, collectionData } from '@angular/fire/firestore';
+import { Firestore, collection, collectionData, docData, addDoc } from '@angular/fire/firestore';
 import { Observable } from 'rxjs';
 import { ActivatedRoute, Router } from '@angular/router';
-import { doc, getDoc } from "firebase/firestore";
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { doc } from "firebase/firestore";
 
 @Component({
   selector: 'app-game',
@@ -21,18 +19,6 @@ export class GameComponent implements OnInit {
   gameDb = collection(this.firestore, 'games');
   currentCard: string = '';
   hasPickCardAnimation = false;
-  // firebaseConfig = {
-  //   apiKey: "AIzaSyDFp0KPMTb0JhBfroWnGRIGUSI86tJyZyo",
-  //   authDomain: "ring-of-fire-e2f80.firebaseapp.com",
-  //   projectId: "ring-of-fire-e2f80",
-  //   storageBucket: "ring-of-fire-e2f80.appspot.com",
-  //   messagingSenderId: "89660740149",
-  //   appId: "1:89660740149:web:dd556651938446989e977c"
-  // };
-  // // Initialize Firebase
-  // app = initializeApp(this.firebaseConfig);
-  // // Initialize Cloud Firestore and get a reference to the service
-  // db = getFirestore(this.app);
   constructor(private route: ActivatedRoute, private router: Router,
     private dialog: MatDialog) {
   }
@@ -42,15 +28,11 @@ export class GameComponent implements OnInit {
     this.route.params.subscribe(async (params) => {
       console.log('ngOnInit(): ' + params['id']);
       // this.games$.doc(params['id']);
-      //this.router.navigateByUrl('/game/' + params['id']);
+      this.router.navigateByUrl('/game/' + params['id']);
       const docRef = doc(collection(this.firestore, 'games'), this.gameDb.id);
-      const docSnap = await getDoc(docRef);
-      if (docSnap.exists()) {
-        console.log("ngOnInit(): Document data:", docSnap.data());
-      } else {
-        // doc.data() will be undefined in this case
-        console.log("ngOnInit(): No such document!");
-      }
+      docData(docRef).subscribe((doc) => {
+        console.log('ngOnInit(): ' + doc);
+      });
     })
     this.startGame();
   }
@@ -58,8 +40,8 @@ export class GameComponent implements OnInit {
 
   async startGame() {
     this.game = new Game();
-    // const gameDb = collection(this.firestore, 'games');
-    // await addDoc(gameDb, { gameJson: this.game.toJson() });
+    const gameDb = collection(this.firestore, 'games');
+    await addDoc(gameDb, { gameJson: this.game.toJson() });
     console.log('startGame(): ' + this.gameDb.id);
     this.games$ = collectionData(this.gameDb);
     this.games$.subscribe((games) => {
